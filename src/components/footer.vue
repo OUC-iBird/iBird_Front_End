81a70114f45e5303f343e34ddc3f986453653851
cyx0706 <3519161997@qq.com>
2020-12-03 00:40:00 +0800

    fix:修复了底部栏适配问题

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,57 +1,40 @@
 <template>
-  <el-container class="footer">
-    <el-footer height="240px">
-      <el-row :gutter="20">
-        <el-col :span="8">
-          <div class="fotter-col">
-            <strong>APP下载</strong>
-            <p>扫描下载iBrid手机客户端</p>
-          </div>
-        </el-col>
-        <el-col :span="8">
-          <div class="fotter-col">
-            <strong>关于我们</strong>
-            <p>关于我们</p>
-            <a href="https://beian.miit.gov.cn" target="_blank">鲁ICP备2020045573号-1</a>
-          </div>
-        </el-col>
-        <el-col :span="8">
-          <div class="fotter-col">
-            <strong>联系我们</strong>
-            <p>点击下方联系我们</p>
-          </div>
-        </el-col>
-      </el-row>
-    </el-footer>
-  </el-container>
+  <div class="footer">
+      <a class="fotter-a" href="https://beian.miit.gov.cn" target="_blank">鲁ICP备2020045573号-1</a>
+      <span class="fotter-text">@ITStudio 2002-2020 All Right Reserved</span>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "ibird-footer"
+  name: "Footer"
 }
 </script>
 
 <style scoped>
 .footer {
+  margin-top: 20px;
   background: #555555;
   color: #bfbfbf;
-  font-size: 1.2em;
-  text-align: left;
-  padding-top: 20px;
-  padding-bottom: 20px;
-  padding-left: 10%;
   width: 100%;
+  /*position: relative;*/
+  bottom: 0;
+  height: 40px;
 }
 
-strong {
-  font-size: 2em;
-  color: #e7e7e7;
+
+.fotter-a{
+  line-height: 20px;
+  margin-top: 10px;
+  float: right;
+  padding-right: 20px;
 }
 
-.fotter-col {
-  padding-left: 5%;
-
+.fotter-text {
+  line-height: 20px;
+  margin-top: 10px;
+  padding-right: 20px;
+  float: right;
 }
 
 a {
